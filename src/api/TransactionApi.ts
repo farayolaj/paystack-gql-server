d7235f1640114186a92d214c01c0b289f8565e8c
Joshua Farayola <farayolajoshua@gmail.com>
2020-12-08 17:33:30 +0100

    feat: transaction api implementation complete

--- src/api/TransactionApi.ts
+++ src/api/TransactionApi.ts
@@ -52,7 +52,64 @@ export async function getTransaction(id: string, apiKey: string) {
 }
 
 export async function getTimeline(id: string, apiKey: string) {
-  const res = await ApiInstance.get(`timeline/${id}`, {
+  const res = await ApiInstance.get(`/timeline/${id}`, {
+    headers: setAuthorizationHeader(apiKey)
+  });
+  return {
+    data: res.data,
+    headers: res.headers,
+    statusCode: res.status
+  };
+}
+
+export async function chargeAuthorization(data: Record<string, any>, apiKey: string) {
+  const res = await ApiInstance.post(`/charge_authorization`, data, {
+    headers: setAuthorizationHeader(apiKey)
+  });
+  return {
+    data: res.data,
+    headers: res.headers,
+    statusCode: res.status
+  };
+}
+
+export async function checkAuthorization(data: Record<string, any>, apiKey: string) {
+  const res = await ApiInstance.post(`/check_authorization`, data, {
+    headers: setAuthorizationHeader(apiKey)
+  });
+  return {
+    data: res.data,
+    headers: res.headers,
+    statusCode: res.status
+  };
+}
+
+export async function getTotals(options: Record<string, any>, apiKey: string) {
+  const res = await ApiInstance.get(`/totals`, {
+    params: options,
+    headers: setAuthorizationHeader(apiKey)
+  });
+  return {
+    data: res.data,
+    headers: res.headers,
+    statusCode: res.status
+  };
+}
+
+export async function exportTransactions(options: Record<string, any>, apiKey: string) {
+  const res = await ApiInstance.get(`/export`, {
+    params: options,
+    headers: setAuthorizationHeader(apiKey)
+  });
+  return {
+    data: res.data,
+    headers: res.headers,
+    statusCode: res.status
+  };
+}
+
+export async function partialDebit(data: Record<string, any>, apiKey: string) {
+  const res = await ApiInstance.post(`/partial_debit`, data, {
     headers: setAuthorizationHeader(apiKey)
   });
   return {
